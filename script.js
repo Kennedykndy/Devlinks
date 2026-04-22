d17bcfd932b42310eca926d8c4b5696df2b91cbb
Kennedykndy <dev.senac.kennedy@gmail.com>
2026-04-22 10:29:21 -0300

    Atualizando e organizando pastas

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle("light");
 
-  //pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img");
 
-  //substituir a imagem
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "/assets/avatar-light.png");
-    //se não tiver light mode, manter a imagem normal
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png");
   } else {
-    img.setAttribute("src", "/assets/avatar.png");
+    // se não tiver light mode, adicionar a imagem dark
+    img.setAttribute("src", "./assets/avatar.png");
   }
 }
